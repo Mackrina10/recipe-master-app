32be1ec6fcd2e1ac825fd4652fee210aafdf5321
Mackrina10 <heavenmhally@gmail.com>
2026-02-09 20:34:37 +0300

    feat(theme): create Material 3 theme with custom colors

--- app/src/main/java/com/example/recipemaster/ui/theme/Color.kt
+++ app/src/main/java/com/example/recipemaster/ui/theme/Color.kt
@@ -2,10 +2,73 @@ package com.example.recipemaster.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * Color definitions for Recipe Master theme
+ * Uses Material 3 color scheme
+ *
+ * @author Heavenlight Mhally
+ */
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Brand colors
+val BrandOrange = Color(0xFFFF6B35)
+val BrandRed = Color(0xFFE63946)
+val BrandYellow = Color(0xFFFFB703)
+val BrandGreen = Color(0xFF06A77D)
+
+// Light theme colors
+val md_theme_light_primary = Color(0xFFFF6B35)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFFFDBD0)
+val md_theme_light_onPrimaryContainer = Color(0xFF3A0A00)
+
+val md_theme_light_secondary = Color(0xFF775651)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFDBD0)
+val md_theme_light_onSecondaryContainer = Color(0xFF2C1512)
+
+val md_theme_light_tertiary = Color(0xFF6B5E2F)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFF5E2A7)
+val md_theme_light_onTertiaryContainer = Color(0xFF231B00)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF201A19)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF201A19)
+val md_theme_light_surfaceVariant = Color(0xFFF5DED7)
+val md_theme_light_onSurfaceVariant = Color(0xFF53433F)
+val md_theme_light_outline = Color(0xFF85736E)
+
+// Dark theme colors
+val md_theme_dark_primary = Color(0xFFFFB59C)
+val md_theme_dark_onPrimary = Color(0xFF5F1600)
+val md_theme_dark_primaryContainer = Color(0xFF7F2A17)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDBD0)
+
+val md_theme_dark_secondary = Color(0xFFE7BDB5)
+val md_theme_dark_onSecondary = Color(0xFF442A25)
+val md_theme_dark_secondaryContainer = Color(0xFF5D3F3B)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDBD0)
+
+val md_theme_dark_tertiary = Color(0xFFD8C68D)
+val md_theme_dark_onTertiary = Color(0xFF3B2F05)
+val md_theme_dark_tertiaryContainer = Color(0xFF534619)
+val md_theme_dark_onTertiaryContainer = Color(0xFFF5E2A7)
+
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF201A19)
+val md_theme_dark_onBackground = Color(0xFFEDE0DD)
+val md_theme_dark_surface = Color(0xFF201A19)
+val md_theme_dark_onSurface = Color(0xFFEDE0DD)
+val md_theme_dark_surfaceVariant = Color(0xFF53433F)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD8C2BB)
+val md_theme_dark_outline = Color(0xFFA08C87)
